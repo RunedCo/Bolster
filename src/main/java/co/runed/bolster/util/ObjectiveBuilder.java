ff6c155c5d9d52b52fcc20e0e8f4014d2a770358
Jono Kemball <Noojuno@users.noreply.github.com>
2020-06-19 18:17:39 +1200

    objectivebuilder changes to stop flickering

--- src/main/java/co/runed/bolster/util/ObjectiveBuilder.java
+++ src/main/java/co/runed/bolster/util/ObjectiveBuilder.java
@@ -10,7 +10,8 @@ import java.util.List;
 public class ObjectiveBuilder {
     Scoreboard scoreboard;
     Objective objective;
-    List<Score> scores = new ArrayList<>();
+    List<String> lines = new ArrayList<>();
+    List<String> bottomLines = new ArrayList<>();
 
     int blankLines = 0;
 
@@ -26,11 +27,11 @@ public class ObjectiveBuilder {
         String id = ChatColor.stripColor(title.toLowerCase());
 
         if(this.scoreboard.getObjective(id) != null) {
-            this.scoreboard.getObjective(id).unregister();
+            this.objective = this.scoreboard.getObjective(id);
+            return this;
         }
 
         this.objective = this.scoreboard.registerNewObjective(id, "dummy", title);
-
         return this;
     }
 
@@ -52,20 +53,86 @@ public class ObjectiveBuilder {
         return this.addLine(new String(new char[this.blankLines]).replace("\0", " "));
     }
 
-    public ObjectiveBuilder addLine(String line) {
-        Score score = this.objective.getScore(line);
-
-        this.scores.add(score);
+    public ObjectiveBuilder addLines(List<String> lines) {
+        for (String line : lines) {
+            this.addLine(line);
+        }
 
         return this;
     }
 
-    public Scoreboard build() {
-        int size = this.scores.size();
-        for (int i = 0; i < size; i++) {
-            Score score = this.scores.get(i);
+    public ObjectiveBuilder addLine(String line) {
+        this.lines.add(line);
 
-            score.setScore(size - 1 - i);
+        return this;
+    }
+
+    public ObjectiveBuilder addLineBottom() {
+        this.blankLines++;
+
+        return this.addLineBottom(new String(new char[this.blankLines]).replace("\0", " "));
+    }
+
+    public ObjectiveBuilder addLineBottom(String line) {
+        this.bottomLines.add(line);
+
+        return this;
+    }
+
+    private List<String> getAllLines() {
+        List<String> scores = new ArrayList<>();
+
+        scores.addAll(this.lines);
+        scores.addAll(this.bottomLines);
+
+        return scores;
+    }
+
+    private List<String> getColors() {
+        List<String> colors = new ArrayList<>();
+
+        return colors;
+    }
+
+    public Scoreboard build() {
+        List<String> lines = this.getAllLines();
+        int lineCount = lines.size();
+
+        /* for (String str : this.scoreboard.getEntries()) {
+            boolean reset = true;
+
+            for (String score : lines) {
+                if(score.equals(str)) {
+                    reset = false;
+                    break;
+                }
+            }
+
+            if(reset) this.scoreboard.resetScores(str);
+        } */
+
+        for (Team team : this.scoreboard.getTeams()) {
+            int count = StringUtil.countMatches(team.getName(), ChatColor.RED.toString());
+
+            if(count > lineCount) team.unregister();
+        }
+
+        for (int i = 0; i < lineCount; i++) {
+            String line = lines.get(i);
+            int value = lineCount - 1 - i;
+
+            String teamId = new String(new char[value + 1]).replace("\0", ChatColor.RED.toString());
+
+            Team team = this.scoreboard.getTeam(teamId);
+
+            if(team == null) {
+                team = this.scoreboard.registerNewTeam(teamId);
+                team.addEntry(teamId);
+
+                this.objective.getScore(teamId).setScore(value);
+            }
+
+            if(!team.getPrefix().equals(line)) team.setPrefix(line);
         }
 
         return this.scoreboard;
