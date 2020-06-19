fdd82c97aa42f56e39ba66b70ddcfa01f16223dc
Jono Kemball <Noojuno@users.noreply.github.com>
2020-06-20 02:30:01 +1200

    Sidebar javadocs

--- src/main/java/co/runed/bolster/sidebar/Sidebar.java
+++ src/main/java/co/runed/bolster/sidebar/Sidebar.java
@@ -22,12 +22,16 @@ public abstract class Sidebar {
     BukkitTask updateTask;
 
     // Sidebar variables
-    String title;
     List<String> lines = new ArrayList<>();
     List<String> bottomLines = new ArrayList<>();
-
     int blankLines = 0;
 
+    /**
+     * Gets the player's scoreboard instance for this sidebar
+     *
+     * @param player The player
+     * @return The scoreboard
+     */
     public Scoreboard getPlayerScoreboard(Player player) {
         Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
 
@@ -42,14 +46,61 @@ public abstract class Sidebar {
         return scoreboard;
     }
 
+    /**
+     * Returns the title of the sidebar
+     *
+     * @return The title
+     */
     public abstract String getTitle();
 
+    /**
+     * Add a blank line to the sidebar
+     *
+     * @return The sidebar instance
+     */
     public Sidebar addLine() {
-        this.blankLines++;
-
-        return this.addLine(StringUtil.repeat(" ", this.blankLines));
+        return this.addLine("");
     }
 
+    /**
+     * Add a line to the sidebar
+     *
+     * @param line The line to add
+     * @return The sidebar instance
+     */
+    public Sidebar addLine(String line) {
+        this.lines.add(line);
+
+        return this;
+    }
+
+    /**
+     * Add a blank line to the sidebar that is always on the bottom
+     *
+     * @return The sidebar instance
+     */
+    public Sidebar addSuffixLine() {
+        return this.addSuffixLine("");
+    }
+
+    /**
+     * Add a line to the sidebar that is always on the bottom
+     *
+     * @param line The line to add
+     * @return The sidebar instance
+     */
+    public Sidebar addSuffixLine(String line) {
+        this.bottomLines.add(line);
+
+        return this;
+    }
+
+    /**
+     * Adds multiple lines to the scoreboard
+     *
+     * @param lines A list of lines to add
+     * @return The sidebar instance
+     */
     public Sidebar addLines(List<String> lines) {
         for (String line : lines) {
             this.addLine(line);
@@ -58,25 +109,12 @@ public abstract class Sidebar {
         return this;
     }
 
-    public Sidebar addLine(String line) {
-        this.lines.add(line);
-
-        return this;
-    }
-
-    public Sidebar addSuffixLine() {
-        this.blankLines++;
-
-        return this.addSuffixLine(StringUtil.repeat(" ", this.blankLines));
-    }
-
-    public Sidebar addSuffixLine(String line) {
-        this.bottomLines.add(line);
-
-        return this;
-    }
-
-    public Sidebar setUpdateInterval(long interval) {
+    /**
+     * Sets the update interval of the sidebar
+     *
+     * @param interval The interval in ticks
+     */
+    public void setUpdateInterval(long interval) {
         long previousInterval = this.getUpdateInterval();
 
         this.updateInterval = interval;
@@ -86,24 +124,32 @@ public abstract class Sidebar {
 
             this.updateTask = this.createUpdateTask();
         }
-
-        return this;
     }
 
+    /**
+     * Returns the update interval of the sidebar
+     *
+     * @return The update interval in ticks
+     */
     public long getUpdateInterval() {
         return this.updateInterval;
     }
 
-    public Sidebar addAllPlayers() {
+    /**
+     * Adds all players to the sidebar causing it to display on their screen
+     */
+    public void addAllPlayers() {
         for (Player player : Bukkit.getOnlinePlayers()) {
             this.addPlayer(player);
         }
-
-        return this;
     }
 
-    public Sidebar addPlayer(Player player) {
-        if(this.players.contains(player)) return this;
+    /**
+     * Adds the player to the sidebar causing it to display on their screen
+     * @param player The player
+     */
+    public void addPlayer(Player player) {
+        if(this.players.contains(player)) return;
 
         this.players.add(player);
 
@@ -114,11 +160,13 @@ public abstract class Sidebar {
                 this.updateTask = this.createUpdateTask();
             }
         }
-
-        return this;
     }
 
-    public Sidebar removePlayer(Player player) {
+    /**
+     * Removes the player from the sidebar causing it to be removed from their screen
+     * @param player The player
+     */
+    public void removePlayer(Player player) {
         this.players.remove(player);
 
         Scoreboard scoreboard = this.getPlayerScoreboard(player);
@@ -135,10 +183,11 @@ public abstract class Sidebar {
         }
 
         player.setScoreboard(Bukkit.getScoreboardManager().getNewScoreboard());
-
-        return this;
     }
 
+    /**
+     * Removes the update task and all the players from the Sidebar
+     */
     public void remove() {
         if(this.updateTask != null) {
             this.updateTask.cancel();
@@ -151,11 +200,21 @@ public abstract class Sidebar {
         }
     }
 
+    /**
+     * Creates the task that runs the {@link #update()} function
+     *
+     * @return The task
+     */
     private BukkitTask createUpdateTask() {
         return Bolster.getInstance().getServer().getScheduler()
                 .runTaskTimer(Bolster.getInstance(), this::update, 0L, this.getUpdateInterval());
     }
 
+    /**
+     * Appends the two row arrays and ensures the bottom rows are on the bottom
+     *
+     * @return All the scoreboard rows
+     */
     private List<String> getAllLines() {
         List<String> scores = new ArrayList<>();
 
@@ -165,6 +224,13 @@ public abstract class Sidebar {
         return scores;
     }
 
+    /**
+     * Gets the objective instance from a scoreboard based on the title
+     *
+     * @param scoreboard The scoreboard
+     * @param title The sidebar title
+     * @return The objective
+     */
     private Objective getOrCreateObjective(Scoreboard scoreboard, String title) {
         String id = ChatColor.stripColor(title.toLowerCase());
         Objective existing = scoreboard.getObjective(id);
@@ -178,15 +244,23 @@ public abstract class Sidebar {
         return scoreboard.registerNewObjective(id, "dummy", title);
     }
 
-    // SPAGHETTI.
+    /**
+     * Resets lines so player specific entries don't show up for everyone.
+     * A little hacky but it should work well enough...
+     * Runs at the end of each update call.
+     */
+    private void resetLines() {
+        this.lines.clear();
+        this.bottomLines.clear();
+        this.blankLines = 0;
+    }
+
+    /**
+     * Handles updating the scoreboard display with the layout defined in {@link #draw(Player)}.
+     * Updates at the tick speed returned from {@link #getUpdateInterval()}
+     */
     public void update() {
         for (Player player : this.players) {
-            // Resets lines so player specific entries don't show up for everyone
-            // A little hacky but it might work well enough...
-            this.lines.clear();
-            this.bottomLines.clear();
-            this.blankLines = 0;
-
             Sidebar sidebar = this.draw(player);
 
             Scoreboard scoreboard = this.getPlayerScoreboard(player);
@@ -226,8 +300,16 @@ public abstract class Sidebar {
 
                 if(!team.getPrefix().equals(line)) team.setPrefix(line);
             }
+
+            this.resetLines();
         }
     }
 
+    /**
+     * Called in {@link #update()} and used to set the player specific sidebar values
+     *
+     * @param player Player to show sidebar to
+     * @return Sidebar instance to be rendered
+     */
     public abstract Sidebar draw(Player player);
 }
