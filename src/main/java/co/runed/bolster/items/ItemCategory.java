67b9547fab6f97a9acd126fcbfba071b0b66bc1f
Jono Kemball <Noojuno@users.noreply.github.com>
2020-06-23 04:28:37 +1200

    add new item categories

--- src/main/java/co/runed/bolster/items/ItemCategory.java
+++ src/main/java/co/runed/bolster/items/ItemCategory.java
@@ -6,8 +6,9 @@ import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 public class ItemCategory {
-    public static final ItemCategory WEAPONS = new ItemCategory("Weapons", null, new ItemStack(Material.DIAMOND_SWORD));
+    public static final ItemCategory WEAPONS = new ItemCategory("Weapons", null, new ItemStack(Material.IRON_SWORD));
     public static final ItemCategory TOOLS = new ItemCategory("Tools", null, new ItemStack(Material.IRON_AXE));
+    public static final ItemCategory MATERIALS = new ItemCategory("Materials", null, new ItemStack(Material.COBBLESTONE));
     public static final ItemCategory ALL = new ItemCategory("All Items", null, new ItemStack(Material.CHEST));
 
     String name;
@@ -16,10 +17,9 @@ public class ItemCategory {
 
     public ItemCategory(String name, String description, ItemStack icon) {
         this.name = name;
-        this.description = description != null ? description : "";
-
+        this.description = description;
         this.icon = new ItemBuilder(icon)
-                .setDisplayName(ChatColor.BOLD + "" + ChatColor.WHITE + name)
+                .setDisplayName(ChatColor.RESET + name)
                 .setLore(this.getDescription())
                 .addAllItemFlags()
                 .build();
