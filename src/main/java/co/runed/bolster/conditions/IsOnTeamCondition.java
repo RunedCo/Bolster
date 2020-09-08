097bab5a49f3b8463eec3426e273066e189c5194
Jono Kemball <Noojuno@users.noreply.github.com>
2020-09-09 02:37:12 +1200

    Create IsOnTeamCondition.java

---
+++ src/main/java/co/runed/bolster/conditions/IsOnTeamCondition.java
@@ -0,0 +1,33 @@
+package co.runed.bolster.conditions;
+
+import co.runed.bolster.conditions.IConditional;
+import co.runed.bolster.conditions.TargetedCondition;
+import co.runed.bolster.game.BolsterEntity;
+import co.runed.bolster.game.Team;
+import co.runed.bolster.util.properties.Properties;
+import co.runed.bolster.util.target.Target;
+
+public class IsOnTeamCondition extends TargetedCondition<BolsterEntity>
+{
+    Team team;
+
+    public IsOnTeamCondition(Target<BolsterEntity> target, Team team)
+    {
+        super(target);
+        this.team = team;
+    }
+
+    @Override
+    public boolean evaluate(IConditional conditional, Properties properties)
+    {
+        BolsterEntity entity = this.getTarget().get(properties);
+
+        return this.team.isInTeam(entity.getBukkit());
+    }
+
+    @Override
+    public void onFail(IConditional conditional, Properties properties)
+    {
+
+    }
+}
